a1086277096f7a711582c98386d936574fad50dd
pmartinsdev <martins20.tech@gmail.com>
2023-05-02 14:28:41 -0300

    fix: resolve metadata error

--- packages/storybook/angular/stories/button.stories.ts
+++ packages/storybook/angular/stories/button.stories.ts
@@ -1,12 +1,10 @@
 import { StoryObj, Meta } from "@storybook/angular"
 import { ButtonComponent } from "@monorepo/angular"
 
-import { resolveResourcesDecorator } from "../.storybook/resolve-resources.decorator"
 
 export default {
   title: "Form/Button",
   component: ButtonComponent,
-  decorators: [resolveResourcesDecorator]
 } as Meta<ButtonComponent>
 
 export const Default: StoryObj<ButtonComponent> = {
